e26f01e114c9557e38f90e3a2de2a839db3ccec6
Prasanna Jena <devusers.datarynx@gmail.com>
2020-11-30 11:53:13 +0530

    web Client first commit

---
+++ src/main/java/com/classic/booking/api/BookMyShowApplication.java
@@ -0,0 +1,13 @@
+package com.classic.booking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowApplication.class, args);
+	}
+
+}
